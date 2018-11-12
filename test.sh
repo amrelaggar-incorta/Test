56ab577a6c1b0b1eafd7dd45f2220ef7dc6649a1
Amr ElNaggar <43375671+amrelaggar-incorta@users.noreply.github.com>
2018-11-12 13:40:48 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,4 @@ cd $SourcePath
 SourceDirSize= $(VAR=`du -shb . | awk '{print $1}'`; VAR2=$(for i in `ls -l | grep '\->' | awk '{print $11}'`; do du -shb $i; done | awk '{total +=$1}; END { print total }';) ; if [ ! -z $VAR2 ] ; then  expr $VAR + $VAR2; else echo $VAR; fi)
 
 echo $SourceDirSize
+#new change1
