68ea8fb764fdd183fe6e7e06c435ba45045e62c3
Amr ElNaggar <43375671+amrelaggar-incorta@users.noreply.github.com>
2018-11-12 12:51:03 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script Inputs
+SourcePath=$1
+ 
+cd $SourcePath
+ 
+SourceDirSize= $(VAR=`du -shb . | awk '{print $1}'`; VAR2=$(for i in `ls -l | grep '\->' | awk '{print $11}'`; do du -shb $i; done | awk '{total +=$1}; END { print total }';) ; if [ ! -z $VAR2 ] ; then  expr $VAR + $VAR2; else echo $VAR; fi)
+
+echo $SourceDirSize
